029dd69e6f889a6b5761318320acd9512ef01e99
zidisw <wijaogyzis20d@student.unhas.ac.id>
2023-04-05 15:14:13 +0800

    update_import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:brain_school/main.dart';
+import 'package:biking_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
