d13aaecb7764b9e81f723e656db960e2b17a2473
zidisw <wijaogyzis20d@student.unhas.ac.id>
2023-06-06 10:35:49 +0800

    Changes to be committed: 	modified:   lib/screens/pelaporan/guru/daftar_pelaporan_guru.dart

--- lib/screens/pelaporan/guru/daftar_pelaporan_guru.dart
+++ lib/screens/pelaporan/guru/daftar_pelaporan_guru.dart
@@ -49,96 +49,98 @@ class _DaftarPelaporanScreenState extends State<DaftarPelaporanScreen> {
         ),
       ),
       backgroundColor: const Color(0xFFFFFFFF),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 13, left: 20),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Column(
-                  children: [
-                    Text(
-                      "Daftar Kartu Komunikasi",
-                      textAlign: TextAlign.left,
-                      style: GoogleFonts.poppins(
-                        color: const Color(0xFF0579CC),
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 13, left: 20),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Column(
+                    children: [
+                      Text(
+                        "Daftar Kartu Komunikasi",
+                        textAlign: TextAlign.left,
+                        style: GoogleFonts.poppins(
+                          color: const Color(0xFF0579CC),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-            StreamBuilder<QuerySnapshot>(
-              stream: _laporanStream,
-              builder: (BuildContext context,
-                  AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                }
-
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const CircularProgressIndicator();
-                }
-
-                final List<DocumentSnapshot> documents = snapshot.data!.docs;
-
-                return ListView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: documents.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    final Map<String, dynamic> data =
-                        documents[index].data() as Map<String, dynamic>;
-                    final Timestamp timestamp = data['Tanggal'] as Timestamp;
-                    final DateTime tanggal = timestamp.toDate();
-
-                    return Card(
-                      child: ListTile(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const IsiKartuScreen(),
-                            ),
-                          );
-                        },
-                        title: Text(
-                          tanggal.toString(),
-                          style: GoogleFonts.poppins(
-                            color: const Color(0xFF000000),
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        subtitle: Text(
-                          "Diproses",
-                          style: GoogleFonts.poppins(
-                            color: const Color(0xFF000000).withOpacity(0.5),
-                            fontSize: 14,
-                            fontWeight: FontWeight.w300,
-                          ),
-                        ),
-                        trailing: IconButton(
-                          icon: const Icon(Icons.edit),
-                          onPressed: () {
+              StreamBuilder<QuerySnapshot>(
+                stream: _laporanStream,
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  }
+      
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const CircularProgressIndicator();
+                  }
+      
+                  final List<DocumentSnapshot> documents = snapshot.data!.docs;
+      
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: documents.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final Map<String, dynamic> data =
+                          documents[index].data() as Map<String, dynamic>;
+                      final Timestamp timestamp = data['Tanggal'] as Timestamp;
+                      final DateTime tanggal = timestamp.toDate();
+      
+                      return Card(
+                        child: ListTile(
+                          onTap: () {
                             Navigator.push(
                               context,
                               MaterialPageRoute(
-                                builder: (context) => const EditKartuScreen(),
+                                builder: (context) => const IsiKartuScreen(),
                               ),
                             );
                           },
+                          title: Text(
+                            tanggal.toString(),
+                            style: GoogleFonts.poppins(
+                              color: const Color(0xFF000000),
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          subtitle: Text(
+                            "Diproses",
+                            style: GoogleFonts.poppins(
+                              color: const Color(0xFF000000).withOpacity(0.5),
+                              fontSize: 14,
+                              fontWeight: FontWeight.w300,
+                            ),
+                          ),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.edit),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const EditKartuScreen(),
+                                ),
+                              );
+                            },
+                          ),
                         ),
-                      ),
-                    );
-                  },
-                );
-              },
-            ),
-          ],
+                      );
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
